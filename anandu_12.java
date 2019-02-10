86b96859a5abb5251a73e297642f9dff11b456a8
AnanduMohan <anandu.tmohan99@gmail.com>
2019-02-10 14:08:01 +0530

    12. Anandu Mohan

---
+++ anandu_12.java
@@ -0,0 +1,23 @@
+import java.util.Scanner;
+
+public class Main {
+	private static Scanner sc;
+	public static void main(String[] args) 
+	{
+		int side, i, j;
+		sc = new Scanner(System.in);
+		
+		System.out.print(" Please Enter number of rows(>2) : ");
+		side = sc.nextInt();	
+			
+		for(i = 1; i <= side; i++){
+		    if(i==side || i==1){
+		        System.out.println();
+		        for(j = 1; j <= side; j++)
+				    System.out.print("1 ");
+		    }
+		    else
+			    System.out.print("\n1"); 
+		}
+	}
+}
